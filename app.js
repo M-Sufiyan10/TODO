01b8638c574184a9872902fd3f4436a567feede6
Sufiyan <m.sufiyanyounis>
2025-08-05 15:08:37 +0500

    bugs: fix some bugs and setup the server

---
+++ app.js
@@ -0,0 +1,52 @@
+// import express from 'express';
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT} and the site is hosted at http://localhost:${PORT}`);
+// }).on('error', (err) => {
+//   if (err.code === 'EADDRINUSE') {
+//     console.error(`Port ${PORT} is already in use. Please try a different port or kill the process using this port.`);
+//     console.error('You can run: lsof -ti:3000 | xargs kill -9');
+//   } else {
+//     console.error('Server error:', err);
+//   }
+//   process.exit(1);
+// });
+
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} and the site is hosted at http://localhost:${PORT}`);
+}).on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${PORT} is already in use. Please try a different port or kill the process using this port.`);
+    console.error('You can run: lsof -ti:3000 | xargs kill -9');
+  } else {
+    console.error('Server error:', err);
+  }
+  process.exit(1);
+});
+
+// Graceful shutdown for nodemon restarts
+function shutdown() {
+  server.close(() => {
+    console.log('Server closed gracefully');
+    process.exit(0);
+  });
+}
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
